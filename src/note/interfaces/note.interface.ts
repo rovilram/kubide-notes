3ce5db93358949b0a4c86987cca02f3ac24e8d32
rovilram <robertovillaresrama@gmail.com>
2021-06-14 23:33:22 +0200

    feat: add note service functions

--- src/note/interfaces/note.interface.ts
+++ src/note/interfaces/note.interface.ts
@@ -1,1 +1,7 @@
-export interface Note {}
+import { Document } from 'mongoose';
+
+export interface Note extends Document {
+  readonly title: string;
+  readonly isFav: boolean;
+  readonly date: Date;
+}
--- src/note/note.service.ts
+++ src/note/note.service.ts
@@ -1,4 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateNoteDTO } from './dto/note.dto';
+import { Note } from './interfaces/note.interface';
 
 @Injectable()
-export class NoteService {}
+export class NoteService {
+  constructor(@InjectModel('Note') private readonly noteModel: Model<Note>) {}
+
+  async getNotes(): Promise<Note[]> {
+    const notes = await this.noteModel.find();
+    return notes;
+  }
+
+  async getNote(idNote: string): Promise<Note> {
+    const note = await this.noteModel.findById(idNote);
+    return note;
+  }
+
+  async createNote(createNoteDTO: CreateNoteDTO): Promise<Note> {
+    const note = await this.noteModel.create(createNoteDTO);
+    return await note.save();
+  }
+
+  async setFav(idNote: string): Promise<Note> {
+    const note = await this.noteModel.findById(idNote);
+
+    return await this.noteModel.findByIdAndUpdate(
+      idNote,
+      {
+        isFav: !note.isFav,
+      },
+      { new: true },
+    );
+  }
+
+  async getFavs(): Promise<Note[]> {
+    const notes = this.noteModel.find({ isFav: true });
+    return notes;
+  }
+}
